01c28cb8af0fcb43bcdf59e7bc62282c5574aca2
Dmitry <Logan6338@mail.ru>
2022-08-17 23:08:38 +0300

    Переделанная задача 1

--- Tasks/Task01/Program.cs
+++ Tasks/Task01/Program.cs
@@ -5,16 +5,26 @@
 
 
 Console.Write("Введите число: ");
-string? number = Console.ReadLine();
+int number = Convert.ToInt32(Console.ReadLine());
+string copy = number.ToString();
+Console.WriteLine(CheckingNumber(copy));
 
-void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
-    Console.WriteLine($"Ваше число: {number} - палиндром.");
+string CheckingNumber(string number)
+{
+  string copy = "";
+  for (int i = number.Length - 1; i >= 0; i--)
+  {
+    copy += number[i];
   }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+  return copy; 
 }
+string result = CheckingNumber(copy);
 
-if (number!.Length == 5){
-  CheckingNumber(number);
+if(result == copy)
+{
+  Console.WriteLine("да");
 }
-else Console.WriteLine($"Введи правильное число");
+else
+{
+  Console.WriteLine("нет");
+}
