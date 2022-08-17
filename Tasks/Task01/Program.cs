559d8b79ea078dcec302639fb238958150424b9a
Dmitry <Logan6338@mail.ru>
2022-08-17 23:28:17 +0300

    Задача 1 написал двумя способами

--- Tasks/Task01/Program.cs
+++ Tasks/Task01/Program.cs
@@ -28,3 +28,18 @@ else
 {
   Console.WriteLine("нет");
 }
+
+/*Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");*/
